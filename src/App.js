02cb680ad7344861d7581d0fcd6f1ddaa79e7125
Fernando <fernandowashimi@gmail.com>
2019-04-17 11:33:21 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    topLeft: 0,
+    topRight: 0,
+    bottomLeft: 0,
+    bottomRight: 0
+  };
+
+  handleTopLeftInputChange = e => this.setState({ topLeft: e.target.value });
+  handleTopRightInputChange = e => this.setState({ topRight: e.target.value });
+  handleBottomLeftInputChange = e =>
+    this.setState({ bottomLeft: e.target.value });
+  handleBottomRightInputChange = e =>
+    this.setState({ bottomRight: e.target.value });
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="main-container">
+        <div
+          id="box"
+          style={{
+            borderTopLeftRadius: this.state.topLeft + "px",
+            borderTopRightRadius: this.state.topRight + "px",
+            borderBottomLeftRadius: this.state.bottomLeft + "px",
+            borderBottomRightRadius: this.state.bottomRight + "px"
+          }}
+        />
+        <div id="input-container">
+          <input
+            id="top-left"
+            type="number"
+            value={this.state.topLeft}
+            onChange={this.handleTopLeftInputChange}
+          />
+          <input
+            id="top-right"
+            type="number"
+            value={this.state.topRight}
+            onChange={this.handleTopRightInputChange}
+          />
+          <input
+            id="bottom-left"
+            type="number"
+            value={this.state.bottomLeft}
+            onChange={this.handleBottomLeftInputChange}
+          />
+          <input
+            id="bottom-right"
+            type="number"
+            value={this.state.bottomRight}
+            onChange={this.handleBottomRightInputChange}
+          />
+        </div>
       </div>
     );
   }
